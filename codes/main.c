8cee6bfce1dd7ba3eabab6334953831c11792e1d
lucasbelo2604 <lucasbgaspardo@gmail.com>
2025-03-20 19:45:08 -0300

    Update main.c

--- codes/main.c
+++ codes/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "funcoes.h"
 
 int main(){
+    Lista *lista = malloc(sizeof(Lista));
+    int opcao;
     
-    return 1;
+    do{
+        opcao = menu();
+
+        if(opcao == 1){
+            Tarefa *nova = cria_tarefa();
+            inserir_tarefa(lista, nova);
+        } else if(opcao == 2){
+            remover_tarefa(lista);
+        }else if(opcao == 3){
+            consultar_tarefa(lista);
+        } else if(opcao == 4){
+            mostra_tarefas(lista);
+        }
+    }while (opcao != 7);
+        
+    
 }
