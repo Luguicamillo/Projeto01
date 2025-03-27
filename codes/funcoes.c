bda894b05711b957d6cfe8b38737769cb67ee507
leofes <141570379+leofes@users.noreply.github.com>
2025-03-27 16:15:59 +0000

    Adicionei oq fizemos ontem + as func. remover e consultar tarefa  agr são buscar_tarefa

--- codes/funcoes.c
+++ codes/funcoes.c
@@ -1,145 +1,158 @@
+
 #include "funcoes.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int inserir_tarefa(Lista *lista, Tarefa *tarefa){
-    if(lista->qtde == 100){
+int inserir_tarefa(Lista *lista, Tarefa *tarefa) {
+    if (lista->qtde >= 100) {
         return 0;
     }
-    lista->vetor[lista->qtde] = tarefa;
-    lista->qtde++;
+    lista->vetor[lista->qtde++] = tarefa;
     return 1;
 }
 
-Tarefa *cria_tarefa(){
+Tarefa *cria_tarefa() {
     Tarefa *nova = malloc(sizeof(Tarefa));
+    if (!nova) {
+        printf("Erro ao alocar memória para a tarefa.\n");
+        exit(1);
+    }
+
     printf("Digite o nome da tarefa: ");
-    scanf("%s", nova->nome);
+    scanf("%19s", nova->nome);
     printf("Digite a prioridade da tarefa: ");
     scanf("%d", &nova->prioridade);
     printf("Digite a duração da tarefa: ");
     scanf("%d", &nova->duracao);
+
     return nova;
 }
 
-void mostra_tarefas(Lista *lista){
-    for(int i = 0; i < lista->qtde; i++){
+void mostra_tarefas(const Lista *lista) {
+    if (lista->qtde == 0) {
+        printf("Nenhuma tarefa cadastrada.\n");
+        return;
+    }
+
+    for (int i = 0; i < lista->qtde; i++) {
         printf("--------------------------\n");
         printf("Nome: \t\t%s\n", lista->vetor[i]->nome);
         printf("Prioridade: \t%d\n", lista->vetor[i]->prioridade);
         printf("Duração: \t%d\n", lista->vetor[i]->duracao);
-        printf("--------------------------\n");
     }
+    printf("--------------------------\n");
 }
 
-int menu(){
+int menu() {
     int opcao;
-    
     printf("--------------------------\n");
-    printf("1 - inserir tarefa\n");
-    printf("2 - remover tarefa\n");
-    printf("3 - consultar tarefa\n");
-    printf("4 - mostrar tarefas\n");
-    printf("5 - ler arquivo\n");
-    printf("6 - gravar arquivo\n");
-    printf("7 - sair\n");
+    printf("1 - Inserir tarefa\n");
+    printf("2 - Remover tarefa\n");
+    printf("3 - Consultar tarefa\n");
+    printf("4 - Mostrar tarefas\n");
+    printf("5 - Ler arquivo\n");
+    printf("6 - Gravar arquivo\n");
+    printf("7 - Sair\n");
     printf("--------------------------\n");
     printf("Digite sua opção: ");
     scanf("%d", &opcao);
-
     return opcao;
 }
 
-void remover_tarefa(Lista *lista){
-    char tarefa[20];
-    int i;
-    printf("--------------------------\n");
-    printf("Digite o nome da tarefa a ser removida: ");
-    scanf("%s", tarefa);
-    for(i = 0; i < lista->qtde; i++){
-        if(strcmp(lista->vetor[i]->nome, tarefa) == 0){
-            break;
+int buscar_tarefa(const Lista *lista, const char *nome) {
+    for (int i = 0; i < lista->qtde; i++) {
+        if (strcmp(lista->vetor[i]->nome, nome) == 0) {
+            return i;
         }
     }
+    return -1;
+}
 
-    if(i == lista->qtde){
-        printf("--------------------------\n");
-        printf("Tarefa não encontrada\n");
-        printf("--------------------------\n");
+void remover_tarefa(Lista *lista) {
+    char nome[20];
+    printf("Digite o nome da tarefa a ser removida: ");
+    scanf("%19s", nome);
+
+    int i = buscar_tarefa(lista, nome);
+    if (i == -1) {
+        printf("Tarefa não encontrada.\n");
         return;
     }
 
     free(lista->vetor[i]);
-
-    for(int j = i; j < lista->qtde - 1; j++){
-        lista->vetor[j] = lista->vetor[j + 1];
-    }
+    memmove(&lista->vetor[i], &lista->vetor[i + 1], (lista->qtde - i - 1) * sizeof(Tarefa *));
     lista->qtde--;
 
-    printf("\nTarefa removida com sucesso\n");
-    printf("--------------------------\n");
-
+    printf("Tarefa removida com sucesso!\n");
 }
 
-void consultar_tarefa(Lista *lista){
-    char tarefa[20];
-    int i;
-    printf("--------------------------\n");
+void consultar_tarefa(const Lista *lista) {
+    char nome[20];
     printf("Digite o nome da tarefa a ser consultada: ");
-    scanf("%s", tarefa);
-    for(i = 0; i < lista->qtde; i++){
-        if(strcmp(lista->vetor[i]->nome, tarefa) == 0){
-            printf("--------------------------\n");
-            printf("Nome: \t\t%s\n", lista->vetor[i]->nome);
-            printf("Prioridade: \t%d\n", lista->vetor[i]->prioridade);
-            printf("Duração: \t%d\n", lista->vetor[i]->duracao);
-            printf("--------------------------\n");
-            break;
-        }
-    }
+    scanf("%19s", nome);
 
-    if(i == lista->qtde){
-        printf("--------------------------\n");
-        printf("Tarefa não encontrada\n");
-        printf("--------------------------\n");
+    int i = buscar_tarefa(lista, nome);
+    if (i == -1) {
+        printf("Tarefa não encontrada.\n");
         return;
     }
 
+    printf("--------------------------\n");
+    printf("Nome: \t\t%s\n", lista->vetor[i]->nome);
+    printf("Prioridade: \t%d\n", lista->vetor[i]->prioridade);
+    printf("Duração: \t%d\n", lista->vetor[i]->duracao);
+    printf("--------------------------\n");
 }
 
-void gravar_arquivo(Lista *lista) {
+void escrever_tarefa(FILE *arquivo, Tarefa *tarefa) {
+    fprintf(arquivo, "%s\n%d\n%d\n", tarefa->nome, tarefa->prioridade, tarefa->duracao);
+}
+
+void gravar_arquivo(const Lista *lista) {
     FILE *arquivo = fopen("tarefas.txt", "w");
-    if (arquivo == NULL) {
+    if (!arquivo) {
         printf("Erro ao abrir o arquivo para gravação.\n");
         return;
     }
 
     for (int i = 0; i < lista->qtde; i++) {
-        fprintf(arquivo, "%s\n", lista->vetor[i]->nome);
-        fprintf(arquivo, "%d\n", lista->vetor[i]->prioridade);
-        fprintf(arquivo, "%d\n", lista->vetor[i]->duracao);
+        escrever_tarefa(arquivo, lista->vetor[i]);
     }
 
     fclose(arquivo);
-    printf("Tarefas gravadas com sucesso no arquivo tarefas.txt.\n");
+    printf("Tarefas gravadas com sucesso!\n");
+}
+
+Tarefa *ler_tarefa(FILE *arquivo) {
+    Tarefa *tarefa = malloc(sizeof(Tarefa));
+    if (!tarefa || fscanf(arquivo, "%19s %d %d", tarefa->nome, &tarefa->prioridade, &tarefa->duracao) != 3) {
+        free(tarefa);
+        return NULL;
+    }
+    return tarefa;
 }
 
 void ler_arquivo(Lista *lista) {
     FILE *arquivo = fopen("tarefas.txt", "r");
-    if (arquivo == NULL) {
+    if (!arquivo) {
         printf("Erro ao abrir o arquivo para leitura.\n");
         return;
     }
 
-    while (!feof(arquivo)) {
-        Tarefa *tarefa = malloc(sizeof(Tarefa));
-        if (fscanf(arquivo, "%19s", tarefa->nome) != 1) break;
-        fscanf(arquivo, "%d", &tarefa->prioridade);
-        fscanf(arquivo, "%d", &tarefa->duracao);
+    while (lista->qtde < 100) {
+        Tarefa *tarefa = ler_tarefa(arquivo);
+        if (!tarefa) break;
         inserir_tarefa(lista, tarefa);
     }
 
     fclose(arquivo);
-    printf("Tarefas lidas do arquivo tarefas.txt.\n");
+    printf("Tarefas lidas do arquivo.\n");
+}
+
+void liberar_lista(Lista *lista) {
+    for (int i = 0; i < lista->qtde; i++) {
+        free(lista->vetor[i]);
+    }
+    free(lista);
 }
--- codes/main.c
+++ codes/main.c
@@ -4,28 +4,43 @@
 
 int main() {
     Lista *lista = malloc(sizeof(Lista));
+    if (!lista) {
+        printf("Erro ao alocar memória para a lista.\n");
+        return 1;
+    }
     lista->qtde = 0;
 
     int opcao;
     do {
         opcao = menu();
 
-        if (opcao == 1) {
-            Tarefa *nova = cria_tarefa();
-            inserir_tarefa(lista, nova);
-        } else if (opcao == 2) {
-            remover_tarefa(lista);
-        } else if (opcao == 3) {
-            consultar_tarefa(lista);
-        } else if (opcao == 4) {
-            mostra_tarefas(lista);
-        } else if (opcao == 5) {
-            ler_arquivo(lista);
-        } else if (opcao == 6) {
-            gravar_arquivo(lista);
+        switch (opcao) {
+            case 1:
+                inserir_tarefa(lista, cria_tarefa());
+                break;
+            case 2:
+                remover_tarefa(lista);
+                break;
+            case 3:
+                consultar_tarefa(lista);
+                break;
+            case 4:
+                mostra_tarefas(lista);
+                break;
+            case 5:
+                ler_arquivo(lista);
+                break;
+            case 6:
+                gravar_arquivo(lista);
+                break;
+            case 7:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
         }
-
     } while (opcao != 7);
 
+    liberar_lista(lista);
     return 0;
 }
